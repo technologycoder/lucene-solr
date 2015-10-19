a344ae26ed13aa78e7a93cb06f08724d8481b38a
Ramsey Haddad <RHADDAD15@bloomberg.net>
2015-10-19 10:43:50 -0400

    Create a WildcardNearQueryBuilder to use WildcardNearQueryParser
    Summary: We need to build a WildcardNearQueryBuilder.
    
    Test Plan: We will test it in dev and in alpha with unittests and queries from Wildebst.
    
    Reviewers: ksrivisa
    
    Reviewed By: ksrivisa
    
    Differential Revision: https://all.phab.dev.bloomberg.com/D371654

---
+++ lucene/queryparser/src/java/org/apache/lucene/queryparser/xml/builders/WildcardNearQueryBuilder.java
@@ -0,0 +1,48 @@
+package org.apache.lucene.queryparser.xml.builders;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.queryparser.xml.ParserException;
+import org.apache.lucene.queryparser.xml.QueryBuilder;
+import org.apache.lucene.queryparser.xml.DOMUtils;
+import org.w3c.dom.Element;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* Builder for keyword phrases specially for wildcarded multi term queries.
+ * Text phrases can be thrown into this builder to get tokenized to form OrderedNearQuery of sub queries of individual tokens. 
+ * Currently this can result in WildcardQuery,PrefixQuery and TermQuery as its sub queries. */
+
+public class WildcardNearQueryBuilder implements QueryBuilder {
+  
+  protected Analyzer analyzer;
+  
+  public WildcardNearQueryBuilder(Analyzer analyzer) {
+    this.analyzer = analyzer;
+  }
+  
+  @Override
+  public Query getQuery(Element e) throws ParserException {
+    String field = DOMUtils.getAttributeWithInheritanceOrFail(e, "fieldName");
+    String text = DOMUtils.getText(e);
+    boolean ignoreWildcard = DOMUtils.getAttribute(e, "ignoreWC", false);
+    WildcardNearQueryParser p = new WildcardNearQueryParser(field, analyzer);
+    return p.parse(text,ignoreWildcard);
+  }
+    
+}
